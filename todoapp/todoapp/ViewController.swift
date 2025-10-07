7cf766bd4f9c4ab9a4e1d1ba7f49d3380d29ff3e
Евгений Овчинников <indlns@MacMini-indlns.lan>
2025-10-07 20:46:25 +0500

    init project

---
+++ todoapp/todoapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  todoapp
+//
+//  Created by Евгений Овчинников on 07.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
